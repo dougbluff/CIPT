184ea3fe1e8d73e5d386ba2eac3378b97b96da3d
Douglas Bluff <bluff.doug@gmail.com>
2016-07-24 23:05:39 -0500

    Changes to Header CSS

---
+++ config.rb
@@ -0,0 +1,9 @@
+
+css_dir = "wp-content/themes/consolidatedinsurancepartners/stylesheets" # by Compass.app 
+sass_dir = "wp-content/themes/consolidatedinsurancepartners/sass" # by Compass.app 
+images_dir = "images" # by Compass.app 
+output_style = :expanded # by Compass.app 
+relative_assets = false # by Compass.app 
+line_comments = true # by Compass.app 
+sass_options = {:debug_info=>false} # by Compass.app 
+sourcemap = false # by Compass.app 
